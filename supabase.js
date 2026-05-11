9c4111831d502b74d973159489774dd375d0c28b
RamonRalf <ramonralf0@outlook.com>
2026-05-11 15:27:23 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://oniddekzivbgsayxufka.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uaWRkZWt6aXZiZ3NheXh1ZmthIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0NDMxNTMsImV4cCI6MjA5NDAxOTE1M30.ndGrmZVHobASZE_Xm_PKZmeml7-zeFEze4bU6k3wBMk'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(supabaseUrl, supabaseKey)
