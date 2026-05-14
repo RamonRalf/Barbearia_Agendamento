72e03076ab990be01aa8fb5ffe6b44d5811ff818
RamonRalf <ramonralf0@outlook.com>
2026-05-13 21:52:33 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // Nome do repositório no GitHub Pages
-  base: '/Barbearia_Agendamento/',
-})
