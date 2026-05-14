cc59b8d04890734738d041c4691d57981fea5e71
RamonRalf <ramonralf0@outlook.com>
2026-05-14 17:58:21 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
+const cacheName = 'barber-v1';
+const assets = ['./', './index.html'];
+
+// Instala o Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// Faz o app funcionar offline (necessário para o botão instalar)
 self.addEventListener('fetch', (event) => {
-  // Isso permite que o app funcione até com internet oscilando
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
